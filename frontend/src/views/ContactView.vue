f387f712d3f0dc937f4ba14799d3e813c0dbba34
Danijel <31479093+ethernal12@users.noreply.github.com>
2022-05-26 14:48:28 +0200

    Adding .htaccess.

--- frontend/src/views/ContactView.vue
+++ frontend/src/views/ContactView.vue
@@ -1,10 +1,10 @@
 
 <template>
   <div class="container px-5 my-5">
-    <form action="https://formspree.io/f/mzboywvp" method="POST" id="contactForm" data-sb-form-api-token=be6a58b7-f669-4ee4-8f8d-5e1989a93ee9>
+    <form action="https://formspree.io/f/mzboywvp" method="POST" id="contactForm" >
       <h3>Get in touch with me!</h3>
       <div class="form-floating mb-3">
-        <input class="form-control" id="emailAddress" type="email" placeholder="email Address" name="email" data-sb-validations="required,email"  />
+        <input class="form-control" id="emailAddress" type="email" placeholder="email Address" name="email" required />
 
         <label for="emailAddress" style="color: black">Your email address:</label>
         <div class="invalid-feedback" data-sb-feedback="emailAddress:required">
@@ -17,24 +17,13 @@
       </div>
       <div class="form-floating mb-3">
         <textarea class="form-control" id="message" name="message" placeholder="message"  type="text" style="height: 10rem;"
-          data-sb-validations="required">
+           required>
 </textarea>
         <label style="color:black" for="message">Your message:</label>
-        <div class="invalid-feedback" data-sb-feedback="message:required">
-          Message is required.
-        </div>
+        
       </div>
-      <div class="d-none" id="submitSuccessMessage">
-        <div class="text-center mb-3">
-          <div class="fw-bolder">Form submission successful!</div>
-          <p>To activate this form, sign up at</p>
-          <a
-            href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-        </div>
-      </div>
-      <div class="d-none" id="submitErrorMessage">
-        <div class="text-center text-danger mb-3">Error sending message!</div>
-      </div>
+ 
+    
       <div class="d-grid">
         <button class="btn btn-primary btn-lg" id="submitButton" type="submit">
           Submit
